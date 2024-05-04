467f64c7e534dbdc969dd607f02d6e438c61a023
mamashell <racrobotham@yahoo.com>
2024-05-04 02:30:23 -0400

    First commit! Look at me go

---
+++ Mixtape.py
@@ -0,0 +1,20 @@
+# Write code below 💖
+
+# 💿 Theme: Video Game Music
+# Enjoy this mixtape of absolute bangers from Video Games like Minecraft, Hollow Knight, Mario, and much more!
+
+playlist = [
+  'ConcernedApe - Dance of the Moonlight Jellies',
+  'Lena Raine - Pigstep',
+  'Super Mario 64 - Dire Dire Docks',
+  'Lena Raine - Starjump',
+  'Christopher Larkin - Fungal Wastes',
+  'Christopher Larkin - Nightmare King',
+  'Pokemon Diamond & Pearl - Azelf, Uxie, or Mespirit Battle',
+  'Super Mario 3D World - Beach Level',
+  'HOYO-MIX - Caelestinum Finale Termini',
+  'Nier Automata - Pascal - Arranged by Ryu Kawamura'
+]
+
+for i in range(len(playlist)):
+  print(playlist[i])
